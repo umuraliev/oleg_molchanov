918e03cf041fd0108ef1c3252f98b2b3b0ef255a
miracle <umuraliev.r@gmail.com>
2022-04-27 08:33:28 +0600

    made detail page and link

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def posts_list(request):
-    n = ['Rus', 'Nick', 'John', 'Sam']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
