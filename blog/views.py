63d37cd54e64fce7f6ca0234ac571ff150f79dac
miracle <umuraliev.r@gmail.com>
2022-04-27 11:22:18 +0600

    Mixins

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Tag
+from django.views.generic import View
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
@@ -8,16 +9,16 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
 
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
-
-
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
