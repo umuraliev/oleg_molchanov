2c43580f0cba81cdf25257853875e896d398b328
miracle <umuraliev.r@gmail.com>
2022-04-28 07:42:07 +0600

    made Delete form and Mixin

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post, Tag
 from django.views.generic import View
 from .utils import *
 from .forms import TagForm, PostForm
+from django.urls import reverse
 
 
 def posts_list(request):
@@ -26,6 +27,12 @@ class PostUpdate(ObjectUpdateMixin, View):
     template = 'blog/post_update.html'
 
 
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete.html'
+    redirect_url = 'posts_list_url'
+
+
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
@@ -45,17 +52,9 @@ class TagUpdate(ObjectUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update.html'
-    # def get(self, request, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     bound_form = TagForm(instance=tag)
-    #     return render(request, 'blog/tag_update.html', context={'form': bound_form, 'tag': tag})
-    #
-    # def post(self, request, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     bound_form = TagForm(request.POST, instance=tag)
-    #
-    #     if bound_form.is_valid():
-    #         new_tag = bound_form.save()
-    #         return redirect(new_tag)
-    #     return render(request, 'blog/tag_update.html/', context={'form': bound_form, 'tag': tag})
 
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete.html'
+    redirect_url = 'tags_list_url'
